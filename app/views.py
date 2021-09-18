ec109dc99ac9be472ff65c5d5da9032d2c8c481c
REDpapa <akamine.sigoto@gmail.com>
2021-09-18 20:45:59 +0900

    諸々作成

--- app/views.py
+++ app/views.py
@@ -1,3 +1,11 @@
+from django.http import request
 from django.shortcuts import render
+from django.views.generic import View
 
+
+# NOTE:app/index.htmlが呼び出されるとからをリターン
 # Create your views here.
+class IndexView(View):
+    def get(self, reqest, **args, **kwargs):
+        return render(request, 'app/index.html', {
+        })
